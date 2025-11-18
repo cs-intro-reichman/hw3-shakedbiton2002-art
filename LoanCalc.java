7b017f59cf563ef52eac98cd326358013575f967
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-11-18 12:29:57 +0200

    test 2

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,10 +28,11 @@ public class LoanCalc {
 	
 	public static double endBalance(double loan, double rate, int n, double payment) {
     double balance = loan;
-    double r = (rate / 100.0) / 12.0; 
+    double r = (rate / 100.0) ; 
 
     for (int i = 0; i < n; i++) {
-        balance = balance * (1 + r) - payment;
+		double balanceAfter = balance - payment;
+        balance = balanceAfter * (1 + r) ;
     }
 
     return balance;
@@ -40,7 +41,7 @@ public class LoanCalc {
 
 
 public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-    double G = 0; 
+    double G = loan / n; 
     iterationCounter = 0;
 
    
@@ -59,25 +60,21 @@ public static double bisectionSolver(double loan, double rate, int n, double eps
     iterationCounter = 0; //איפוס מספור האינטרקציות 
     double L = loan / n; 
     double H = loan; 
-    double G = (L + H) / 2;
+    double G = 0;
 
     while ((H - L) > epsilon) {
-
-        double fL = endBalance(loan, rate, n, L);
-        double fG = endBalance(loan, rate, n, G);
-
-        iterationCounter++;
-
-        
-        if (fG * fL > 0) {
+		iterationCounter++;
+		G = (L + H) / 2; // נקודת אמצע חדשה
+        double currentbalance = endBalance(loan, rate, n, G);
+        if (currentbalance > 0) {
             L = G;
         } else {
             H = G;
         }
 
-        G = (L + H) / 2; // נקודת אמצע חדשה
+        
     }
 
-    return G;
+    return (L + H) / 2;
 }
 }
